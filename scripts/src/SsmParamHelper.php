6d14e2bf8577aa6e566be18a82ddf0fb1636deeb
Sam <sam@expertcoder.io>
2024-06-24 14:54:59 +0300

    SP-4120 SP-4109 AWS Multi Region Support

--- scripts/src/SsmParamHelper.php
+++ scripts/src/SsmParamHelper.php
@@ -15,23 +15,30 @@ class SsmParamHelper
 
     protected function getSsmClient(): SsmClient
     {
-        // TODO-SAM tidy up/remove any unneeded "provider" related code/comments
-        // Use AWS SSO Profile called "zitcha". This is still configured in $HOME/.aws but is slightly different ot a standard profile
-        // You also need to log into AWS CLI SSO on the host with `aws sso login --profile zitcha`
-        // $provider = CredentialProvider::sso('zitcha');
-
         $args = [
-//            'region' => 'ap-southeast-2', // SDK didn't seem to get the region from the environmental var automatically
             'version' => '2014-11-06',
-//            'credentials' => $provider
         ];
 
-//        if (!empty($_ENV['AWS_PROFILE'])) {
-//            $args['profile'] = $_ENV['AWS_PROFILE'];
-//        }
+        return new SsmClient($args);
+    }
 
-        // TODO-SAM hacked profile
-//        $args['profile'] = 'zitcha-iam-user';
+    /**
+     * @internal Currently not used, but might be useful on occasion for testing from local dev
+     */
+    protected function getSsmClientForLocalDev(): SsmClient
+    {
+        // Use AWS SSO Profile called "zitcha". This is still configured in $HOME/.aws but is slightly different ot a standard profile
+        // You also need to log into AWS CLI SSO on the host with `aws sso login --profile zitcha`
+        $provider = CredentialProvider::sso('zitcha');
+
+        $args = [
+            'version' => '2014-11-06',
+            'credentials' => $provider
+        ];
+
+        if (!empty($_ENV['AWS_PROFILE'])) {
+            $args['profile'] = $_ENV['AWS_PROFILE'];
+        }
 
         return new SsmClient($args);
     }
